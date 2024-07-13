2c5f305813ff719fe6aad5c1f4544306358a7e1c
Yuanshine <demwre@outlook.com>
2024-07-13 20:59:51 +0800

    初次体验WidgetKit

---
+++ NahidaWidget/NahidaWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NahidaWidgetBundle.swift
+//  NahidaWidget
+//
+//  Created by 鸳汐 on 2024/7/13.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NahidaWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NahidaWidget()
+    }
+}
---
+++ NahidaWidget/TextBaseWidget.swift
@@ -0,0 +1,8 @@
+//
+//  TextBaseWidget.swift
+//  NahidaWidgetExtension
+//
+//  Created by 鸳汐 on 2024/7/13.
+//
+
+import Foundation
