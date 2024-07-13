08ee1e1dd63a956c67c1ad43c7cc669d4fe02b6b
Yuanshine <demwre@outlook.com>
2024-07-13 17:36:59 +0800

    Initial Commit

---
+++ NahidaInDesktop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NahidaInDesktop
+//
+//  Created by 鸳汐 on 2024/7/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NahidaInDesktop/NahidaInDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  NahidaInDesktopApp.swift
+//  NahidaInDesktop
+//
+//  Created by 鸳汐 on 2024/7/13.
+//
+
+import SwiftUI
+
+@main
+struct NahidaInDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
